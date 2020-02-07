253a26481914e4577d3d3a1d313d0addae14af79
JimBaba <aalberts.daniel@gmail.com>
2020-02-07 23:14:30 +0100

    added array.js

---
+++ arrays.js
@@ -0,0 +1,50 @@
+let abk = ["lol", "rofl", "WTF", "DTF", "GTL"];
+let answer = "DTF";
+
+console.log(abk, abk.length)
+
+abk.push("BRB");
+
+console.log(abk, abk.length);
+
+abk.push("re", "LFG")
+
+console.log(abk, abk.length);
+
+abk.pop();
+
+console.log(abk, abk.length);
+
+abk.unshift("wb");
+
+console.log(abk, abk.length);
+
+abk.unshift("LFR", "WTS", "WTB")
+
+console.log(abk, abk.length);
+
+abk.shift();
+
+console.log(abk, abk.length);
+
+let curses = ["fuck", "idiot", "shit"];
+
+console.log(curses, curses.length);
+
+let bothArrays = curses.concat(abk);
+
+console.log(bothArrays, bothArrays.length);
+
+let userInput = "np";
+
+if(bothArrays.includes(userInput)) {
+    console.log("Already in the array");
+} else {
+    bothArrays.push(userInput);
+    console.log(bothArrays, bothArrays.length);
+}
+
+let check = "WTF";
+
+console.log(bothArrays.indexOf(check));
+
--- scripts.js
+++ scripts.js
@@ -1,4 +1,34 @@
-let name = "Jim";
-let lastName = "Payne";
+let highScore = 1600;
+let userScore = 1903;
 
-alert("Your name is " + name + " " + lastName + " ,correct?");
+if (userScore > highScore) {
+    if (userScore % 2 !== 0) {
+        console.log(`Congratz, you set the new ODD Highscore of 
+        ${userScore} Points and beat the old Highscore by ${userScore - highScore} Points`)
+    }
+    else {
+        console.log(`Congratz, you set the new EVEN Highscore of 
+        ${userScore} Points and beat the old Highscore by ${userScore - highScore} Points`)
+    }
+}
+else if (userScore < highScore) {
+    console.log(`Hahahaha, you only got ${userScore} Points! The Highscore is ${highScore} Points`)
+}
+else {
+    console.log(`The Highscore was ${highScore} Points and your score was ${userScore}, that is awkward.`)
+}
+
+let password = "hu t";
+
+if(password.length >= 6 && password.indexOf(" ") === -1) {
+    console.log("Password valid");
+}
+else if(password.length >= 6 && password.indexOf(" ") !== -1){
+    console.log("No Spaces allowed");
+}
+else if (password.length < 6 && password.indexOf(" ") === -1) {
+    console.log("You password is too short");
+}
+else {
+    console.log("You need a password length of at least 6 and no spaces allowed");
+}
